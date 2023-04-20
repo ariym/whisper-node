01dc7ea54e251fa29b72f59b90e199d03fb6ee4d
Ari <ari@mangotap.com>
2023-04-20 02:45:29 -0400

    added test script to reload during dev

---
+++ src/test.ts
@@ -0,0 +1,19 @@
+import {whisper} from './index';
+(async function run() {
+  try {
+
+    const transcript = await whisper(
+      "/Users/Shared/twospeak_clip.wav",
+      {
+        // modelPath: "/Users/Shared/custom-models/ggml-base.en.bin",
+        modelName: "base.en",
+        whisperOptions: { word_timestamps: true }
+      }
+    );
+
+    console.log("transcript", transcript);
+
+  } catch (error) {
+    console.log("ERROR", error);
+  }
+})()
